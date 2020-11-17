753c9cc97253d3aac450f76a392599a5bfd59dab
SheillyR <sheillyrolo@gmail.com>
2020-11-17 12:40:07 -0500

    Installed jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,20 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
   ],
+  plugins: ['jest'],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
